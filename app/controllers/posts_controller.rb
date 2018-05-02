4e5dcf0e255fed25f83732d44977c8fde1d0764f
我妻　伶治 <otete1996@yahoo.co.jp>
2018-05-02 17:06:52 +0900

    refactring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post,only:[:show,:edit,:update,:destroy]
   def index
     @posts=Post.all
     @new_posts=Post.all
@@ -6,7 +7,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post=Post.find(params[:id])
 
   end
 
@@ -22,18 +22,15 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post=Post.find(params[:id])
 
   end
 
   def update
-    @post=Post.find(params[:id])
     @post.update(post_params)
     redirect_to(post_path(@post))
   end
 
   def destroy
-    @post=Post.find(params[:id])
     @post.destroy
     redirect_to(posts_path)
   end
@@ -42,5 +39,8 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title,:body,:category)
+  end
+  def set_post
+    @post=Post.find(params[:id])
   end
 end
